62725b32059c6f77b47dcf31ef8b6b76cba9c361
Gordhan Suthar <gdnagal1536@gmail.com>
2024-02-10 15:58:43 +0530

    jdg

--- connection.php
+++ connection.php
@@ -19,22 +19,23 @@ if($type == "LOCALDB"){
 
 $allowedOrigins = [
   "http://localhost:3000",
+  "https://localhost:3000",
   "http://localhost",
   "https://royalplay.live", 
 ];
 
 
-// if (isset($_SERVER['HTTP_ORIGIN']) && in_array($_SERVER['HTTP_ORIGIN'], $allowedOrigins)) {
-//   header("Access-Control-Allow-Origin: " . $_SERVER['HTTP_ORIGIN']);
-//   header('Access-Control-Allow-Credentials: true');
-//   header('Access-Control-Max-Age: 86400');
-//   header("Access-Control-Allow-Headers: Authorization");
-// }
+if (isset($_SERVER['HTTP_ORIGIN']) && in_array($_SERVER['HTTP_ORIGIN'], $allowedOrigins)) {
+  header("Access-Control-Allow-Origin: " . $_SERVER['HTTP_ORIGIN']);
+  header('Access-Control-Allow-Credentials: true');
+  header('Access-Control-Max-Age: 86400');
+  header("Access-Control-Allow-Headers: Authorization");
+}
 
-header("Access-Control-Allow-Origin: *");
-header('Access-Control-Allow-Credentials: true');
-header('Access-Control-Max-Age: 86400');
-header("Access-Control-Allow-Headers: Authorization");
+// header("Access-Control-Allow-Origin: *");
+// header('Access-Control-Allow-Credentials: true');
+// header('Access-Control-Max-Age: 86400');
+// header("Access-Control-Allow-Headers: Authorization");
 
 date_default_timezone_set("Asia/Calcutta");
 
