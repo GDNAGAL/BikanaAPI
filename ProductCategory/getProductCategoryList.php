c30a944a88613c83ef96495196eede0248bbe23b
Gordhan Suthar <gdnagal1536@gmail.com>
2024-02-10 16:38:12 +0530

    update

---
+++ ProductCategory/getProductCategoryList.php
@@ -0,0 +1,42 @@
+<?php
+include("../connection.php");
+require("../encryption.php");
+
+if($_SERVER['REQUEST_METHOD'] == 'GET'){
+    $headers = getallheaders();
+	if (array_key_exists('Authorization', $headers) && preg_match('/Bearer\s(\S+)/', $headers['Authorization'], $matches)){
+
+		if(verifyToken($matches[1])){
+
+            $UserID = $LoginUserID;
+
+            $CategoryList = mysqli_query($conn, "SELECT * FROM `product_category`");
+            while($PCRow = mysqli_fetch_assoc($CategoryList)){
+                $CategoryArr = $PCRow;
+            }
+            $data = array ("ProductCategoryList" => $CategoryArr);
+            response(200, $data);
+
+		}else{
+			
+            $data = array ("Message" => "Invalid Token");
+            response(401, $data);
+
+		}
+
+	}else{
+
+		$data = array ("Message" => "Token Not Found");
+        response(401, $data);
+
+	}
+
+	
+}else{
+
+    $data = array ("Message" => "UNAUTHORIZED");
+    response(405, $data);
+	
+}
+
+?>
