4c6263ba0a8f41d7db4e9bcfc38c3eed1e91e6fb
Gordhan Suthar <gdnagal1536@gmail.com>
2024-02-10 16:19:46 +0530

    update

--- connection.php
+++ connection.php
@@ -35,7 +35,7 @@ $allowedOrigins = [
 
 header("Access-Control-Allow-Origin: " . $_SERVER['HTTP_ORIGIN']);
 header('Access-Control-Allow-Credentials: true');
-header('Access-Control-Max-Age: 86400');
+header('Access-Control-Max-Age: 31536000');
 header("Access-Control-Allow-Headers: *");
 header("Access-Control-Allow-Methods: *");
 
