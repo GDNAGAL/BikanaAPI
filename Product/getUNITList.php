c21e8ee098327cfea9c67db7c39e060cc67e9a39
Gordhan Suthar <gdnagal1536@gmail.com>
2024-02-13 20:26:35 +0530

    update

---
+++ Product/getUNITList.php
@@ -0,0 +1,47 @@
+<?php
+include("../connection.php");
+require("../encryption.php");
+
+if($_SERVER['REQUEST_METHOD'] == 'GET'){
+    $headers = getallheaders();
+    
+    $authorizationHeader = trim($headers['Authorization']);
+
+	if (array_key_exists('Authorization', $headers) && preg_match('/Bearer\s(\S+)/', $authorizationHeader, $matches)){
+		if(verifyToken($matches[1])){
+
+            $UserID = $LoginUserID;
+            $UNITArr = [];
+            $UNITList = mysqli_query($conn, "SELECT ID, UnitText FROM `UNIT_units` WHERE isActive = 1");
+            while($PCRow = mysqli_fetch_assoc($UNITList)){
+                
+                $PCRow['UNITID'] = $PCRow['ID'];
+                unset($PCRow['ID']);
+                $UNITArr[] = $PCRow;
+            }
+            $data = array ("UNITList" => $UNITArr);
+            response(200, $data);
+
+		}else{
+			
+            $data = array ("Message" => "Invalid Token");
+            response(401, $data);
+
+		}
+
+	}else{
+
+		$data = array ("Message" => "Token Missing");
+        response(401, $data);
+
+	}
+
+	
+}else{
+
+    $data = array ("Message" => "UNAUTHORIZED");
+    response(405, $data);
+	
+}
+
+?>
