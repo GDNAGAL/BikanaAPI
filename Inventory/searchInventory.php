33b88d9986387d1f615cc6ccbba556c4a79b895a
Gordhan Suthar <gdnagal1536@gmail.com>
2024-02-13 15:46:22 +0530

    update

--- Inventory/searchInventory.php
+++ Inventory/searchInventory.php
@@ -15,14 +15,16 @@ if($_SERVER['REQUEST_METHOD'] == 'GET'){
             $InventoryArr = [];
             $IDinSearch = deCodeID($SearchText, "PI");
             // $InventoryList = mysqli_query($conn, "SELECT pi.ID,pi.Created_By, ProductName, ProductDesc, pi.Created_At, CategoryName, users.Name FROM `product_inventory` as pi INNER JOIN users ON users.ID = pi.Created_By INNER JOIN product_category ON product_category.ID = pi.CategoryID WHERE ProductName LIKE '%$SearchText%'");
-            $InventoryList = mysqli_query($conn, "SELECT ProductName, pi.ID FROM `product_inventory` as pi INNER JOIN users ON users.ID = pi.Created_By INNER JOIN product_category ON product_category.ID = pi.CategoryID WHERE ProductName LIKE '%$SearchText%' OR pi.ID LIKE '%$IDinSearch%'");
+            $InventoryList = mysqli_query($conn, "SELECT ProductName, pi.ID, ProductDesc, pc.ID as CID FROM `product_inventory` as pi INNER JOIN product_category as pc ON pc.ID = pi.CategoryID WHERE ProductName LIKE '%$SearchText%' OR pi.ID LIKE '%$IDinSearch%'");
             while($PCRow = mysqli_fetch_assoc($InventoryList)){
                 
                 $PCRow['InventoryID'] = setCodeID($PCRow['ID'],"PI");
+                $PCRow['CategoryID'] = setCodeID($PCRow['CID'],"PC");
                 // $PCRow['Created_ByID'] = $PCRow['Created_By'];
                 // $PCRow['Created_By'] = $PCRow['Name'];
                 // unset($PCRow['ID']);
                 unset($PCRow['ID']);
+                unset($PCRow['CID']);
                 $InventoryArr[] = $PCRow;
             }
             $data = array ("InventoryList" => $InventoryArr);
