8832bc0fc2075ac09f0a4c47fe36ce73370c2c2f
Gordhan Suthar <gdnagal1536@gmail.com>
2024-02-13 20:14:50 +0530

    update

--- Product/addProduct.php
+++ Product/addProduct.php
@@ -21,6 +21,10 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
 
 
             mysqli_query($conn, "INSERT INTO `products`(`ProductName`, `ProductDesc`, `CategoryID`, `InventoryID`, `PinVariant`, `Created_At`, `Modified_At`, `StoreID`)  VALUES ('$ProductName','$ProductDesc','$CategoryID','$InventoryID','null','$CurrendDateTime','$CurrendDateTime','$UserID')");
+            $ProductID = mysqli_insert_id($conn);
+            mysqli_query($conn, "INSERT INTO `product_variant`(`VariantTitle`, `ProductID`, `UnitID`, `MRP`, `Price`, `AvailableQuantity`, `isActive`)  VALUES ('$VariantTitle','$ProductID','$UnitID','$MRP','$Price','0','1')");
+            $VariantID = mysqli_insert_id($conn);
+            mysqli_query($conn, "UPDATE `products` set `PinVariant`=$VariantID WHERE ID=$ProductID");
             $data = array ("Message" => "Product Added Successfully");
             response(200, $data);
 
