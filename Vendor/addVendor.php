02ef14ddc8d206fbe8ff34f3c4a3970dfe4439c0
Gordhan Suthar <gdnagal1536@gmail.com>
2024-02-13 22:16:09 +0530

    update

--- Vendor/addVendor.php
+++ Vendor/addVendor.php
@@ -17,10 +17,15 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
             $StoreAddress = $_POST['StoreAddress'];
             $StoreUserName = $_POST['StoreUserName'];
             $StorePassword = $_POST['StorePassword'];
+            $userEnc = md5($StoreUserName);
+            $passEnc = md5($StorePassword);
 
             mysqli_query($conn, "INSERT INTO `vendor`(`Name`, `Email`, `Mobile`, `Created_By`, `Created_At`, `Modified_At`, `VendorStatusID`) VALUES ('$Name','$Email','$Mobile','$UserID','$CurrendDateTime','$CurrendDateTime','1')");
             $VendorID = mysqli_insert_id($conn);
-            mysqli_query($conn, "INSERT INTO `vendor_stores`(`VendorID`, `StoreName`, `StoreAddress`, `StoreUserName`, `StorePassword`, `StoreStatusID`, `Created_By`, `Created_At`, `Modified_At`) VALUES ('$VendorID','$StoreName','$StoreAddress','$StoreUserName','$StorePassword','1','$UserID','$CurrendDateTime','$CurrendDateTime')");
+
+            mysqli_query($conn, "INSERT INTO `users`(`Name`, `Mobile`, `Username`, `UsernameEnc`, `PasswordEnc`, `UserGroupID`, `UserStatusID`, `AllowedLogins`) VALUES ('$StoreName','$Mobile',$StoreUserName','$userEnc','$passEnc','2','1','1')");
+            $NewUSerID = mysqli_insert_id($conn);
+            mysqli_query($conn, "INSERT INTO `vendor_stores`(`ID`, `VendorID`, `StoreName`, `StoreAddress`, `StoreUserName`, `StorePassword`, `StoreStatusID`, `Created_By`, `Created_At`, `Modified_At`) VALUES ('$NewUSerID','$VendorID','$StoreName','$StoreAddress','$StoreUserName','$StorePassword','1','$UserID','$CurrendDateTime','$CurrendDateTime')");
             $data = array ("Message" => "Vendor Added Successfully");
             response(200, $data);
 
