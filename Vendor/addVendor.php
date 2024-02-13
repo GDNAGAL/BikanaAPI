d4c8e5ef7d15a7de78c6dcebc90c484e98b8a6de
Gordhan Suthar <gdnagal1536@gmail.com>
2024-02-13 22:21:48 +0530

    update

--- Vendor/addVendor.php
+++ Vendor/addVendor.php
@@ -23,8 +23,8 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
             mysqli_query($conn, "INSERT INTO `vendor`(`Name`, `Email`, `Mobile`, `Created_By`, `Created_At`, `Modified_At`, `VendorStatusID`) VALUES ('$Name','$Email','$Mobile','$UserID','$CurrendDateTime','$CurrendDateTime','1')");
             $VendorID = mysqli_insert_id($conn);
 
-            mysqli_query($conn, "INSERT INTO `users`(`Name`, `Mobile`, `Username`, `UsernameEnc`, `PasswordEnc`, `UserGroupID`, `UserStatusID`, `AllowedLogins`) VALUES ('$StoreName','$Mobile',$StoreUserName','$userEnc','$passEnc','2','1','1')");
-            
+            mysqli_query($conn, "INSERT INTO `users`(`Name`, `Mobile`, `Username`, `UsernameEnc`, `PasswordEnc`, `UserGroupID`, `UserStatusID`, `AllowedLogins`) VALUES ('$StoreName','$Mobile','$StoreUserName','$userEnc','$passEnc','2','1','1')");
+
             $NewUSerID = mysqli_insert_id($conn);
             mysqli_query($conn, "INSERT INTO `vendor_stores`(`ID`, `VendorID`, `StoreName`, `StoreAddress`, `StoreUserName`, `StorePassword`, `StoreStatusID`, `Created_By`, `Created_At`, `Modified_At`) VALUES ('$NewUSerID','$VendorID','$StoreName','$StoreAddress','$StoreUserName','$StorePassword','1','$UserID','$CurrendDateTime','$CurrendDateTime')");
             $data = array ("Message" => "Vendor Added Successfully");
