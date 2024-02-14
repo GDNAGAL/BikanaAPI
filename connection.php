16a7edd8bf6a7baace486de3d1a5ca1febf1cea4
Gordhan Suthar <gdnagal1536@gmail.com>
2024-02-14 16:38:11 +0530

    jdg

--- connection.php
+++ connection.php
@@ -17,21 +17,21 @@ if($type == "LOCALDB"){
 
 
 
-$allowedOrigins = [
-  "http://localhost:3000",
-  "http://localhost:3000/",
-  "http://localhost",
-  "https://royalplay.live", 
-];
+// $allowedOrigins = [
+//   "http://localhost:3000",
+//   "http://localhost:3000/",
+//   "http://localhost",
+//   "https://royalplay.live", 
+// ];
 
 
-if (isset($_SERVER['HTTP_ORIGIN']) && in_array($_SERVER['HTTP_ORIGIN'], $allowedOrigins)) {
-  header("Access-Control-Allow-Origin: " . $_SERVER['HTTP_ORIGIN']);
-  header('Access-Control-Allow-Credentials: true');
-  header('Access-Control-Max-Age: 86400');
-  header("Access-Control-Allow-Headers: Content-Type, Authorization");
-  header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
-}
+// if (isset($_SERVER['HTTP_ORIGIN']) && in_array($_SERVER['HTTP_ORIGIN'], $allowedOrigins)) {
+//   header("Access-Control-Allow-Origin: " . $_SERVER['HTTP_ORIGIN']);
+//   header('Access-Control-Allow-Credentials: true');
+//   header('Access-Control-Max-Age: 86400');
+//   header("Access-Control-Allow-Headers: Content-Type, Authorization");
+//   header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+// }
 
 // header("Access-Control-Allow-Origin: " . $_SERVER['HTTP_ORIGIN']);
 // header('Access-Control-Allow-Credentials: true');
