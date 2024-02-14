4d4625dd613e5874b4b725f7ed6829eea117f19b
Gordhan Suthar <gdnagal1536@gmail.com>
2024-02-14 15:08:04 +0530

    jdg

--- User/UserInfo.php
+++ User/UserInfo.php
@@ -10,6 +10,14 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
 
             $UserID = $LoginUserID;
             $User = mysqli_fetch_assoc(mysqli_query($conn, "SELECT * FROM `users` INNER JOIN user_groups ON user_groups.ID = users.UserGroupID WHERE users.ID = '$UserID'"));
+            $Permissions = mysqli_query($conn, "SELECT PermissionKey FROM `user_group_permissions` WHERE UserGroupID = $User[UserGroupID]");
+            while($PermissionsRow = mysqli_fetch_assoc($Permissions)){
+                $PermissionsArr[] = $PermissionsRow;
+            }
+            $USERPermissions = mysqli_query($conn, "SELECT PermissionKey FROM `users_permissions` WHERE UserID = $User[ID]");
+            while($USERPermissionsRow = mysqli_fetch_assoc($USERPermissions)){
+                $PermissionsArr[] = $USERPermissionsRow;
+            }
             $data = array ("Message" => "Success", "User" => $User);
             response(200, $data);
 
