27d3fd99b0ede34e0ea13d570be40c5f62cc2103
Gordhan Suthar <gdnagal1536@gmail.com>
2024-02-15 15:04:26 +0530

    jdg

---
+++ UserRoles/addPermission.php
@@ -0,0 +1,42 @@
+<?php
+include("../connection.php");
+require("../encryption.php");
+
+if($_SERVER['REQUEST_METHOD'] == 'POST'){
+    $headers = getallheaders();
+	if (array_key_exists('Authorization', $headers) && preg_match('/Bearer\s(\S+)/', $headers['Authorization'], $matches)){
+
+		if(verifyToken($matches[1])){
+
+            $UserID = $LoginUserID;
+            $PermissionKey = $_POST['PermissionKey'];
+            $PermissionText = $_POST['PermissionText'];
+
+
+            mysqli_query($conn, "INSERT INTO `user_groups`(`PermissionKey`, `PermissionText`) VALUES ('$PermissionKey','$PermissionText')");
+            $data = array ("Message" => "Permission Key Added Successfully");
+            response(200, $data);
+
+		}else{
+			
+            $data = array ("Message" => "Invalid Token");
+            response(401, $data);
+
+		}
+
+	}else{
+
+		$data = array ("Message" => "Token Not Found");
+        response(401, $data);
+
+	}
+
+	
+}else{
+
+    $data = array ("Message" => "UNAUTHORIZED");
+    response(405, $data);
+	
+}
+
+?>
