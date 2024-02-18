f9e762136ad91d88dad5b9c0b6021f41a9ae150c
Gordhan Suthar <gdnagal1536@gmail.com>
2024-02-18 10:37:19 +0530

    jdg

---
+++ Product/updateVariantPrice.php
@@ -0,0 +1,51 @@
+<?php
+include("../connection.php");
+require("../encryption.php");
+
+if($_SERVER['REQUEST_METHOD'] == 'POST'){
+    $headers = getallheaders();
+    
+    $authorizationHeader = trim($headers['Authorization']);
+
+	if (array_key_exists('Authorization', $headers) && preg_match('/Bearer\s(\S+)/', $authorizationHeader, $matches)){
+		if(verifyToken($matches[1])){
+
+            $UserID = $LoginUserID;
+            $ProductID = deCodeID($_POST['ProductID'],"PD");
+            $MRP = $_POST['MRP'];
+            $VariantID = $_POST['VariantID'];
+            $Price = $_POST['Price'];
+
+            $updateVariant = mysqli_query($conn, "UPDATE `product_variant` SET `MRP`='$MRP', `Price`='$Price' WHERE ID = '$VariantID' AND ProductID = '$ProductID'");
+            
+            if($updateVariant){   
+                $data = array ("Message" => "Price Updated Successfully");
+                response(200, $data);
+            }else{
+                $data = array ("Message" => "Price Updation Failed");
+                response(401, $data);
+            }
+
+		}else{
+			
+            $data = array ("Message" => "Invalid Token");
+            response(401, $data);
+
+		}
+
+	}else{
+
+		$data = array ("Message" => "Token Missing");
+        response(401, $data);
+
+	}
+
+	
+}else{
+
+    $data = array ("Message" => "UNAUTHORIZED");
+    response(405, $data);
+	
+}
+
+?>
