148fcd9a58ebe5848c64fe176c67954a655ebfb7
Gordhan Suthar <gdnagal1536@gmail.com>
2024-02-21 14:28:08 +0530

    jdg

--- WhatsApp/Inbound.php
+++ WhatsApp/Inbound.php
@@ -2,9 +2,10 @@
 if($_SERVER['REQUEST_METHOD']=="GET"){
     echo $_GET['hub_challenge']; //respond back hub_callenge key
     http_response_code(200);
-}else{
+} else {
     $data = json_decode(file_get_contents('php://input'), true);
-    error_log(json_encode($data)); //print inbound message    
-    print_r($data);
+    error_log(json_encode($data)); //print inbound message     
+    print_r($data); // print the decoded data
 }
+
 ?>
