1752a4826d9bd48ac57d2a0704701cd54ac6e0ae
Gordhan Suthar <gdnagal1536@gmail.com>
2024-02-22 10:04:15 +0530

    update

--- WhatsApp/getConversations.php
+++ WhatsApp/getConversations.php
@@ -15,6 +15,7 @@ if($_SERVER['REQUEST_METHOD'] == 'GET'){
 
             $ConversationList = mysqli_query($conn, "SELECT * FROM `whatsapp_conversation`");
             while($PCRow = mysqli_fetch_assoc($ConversationList)){
+                $PCRow['UnReadMessageCount'] = 4;
                 $ConversationArr[] = $PCRow;
             }
             $data = array ("ConversationList" => $ConversationArr);
