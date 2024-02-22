9f2793b82ac47bc9e44d4e9260c96900a359b2c5
Gordhan Suthar <gdnagal1536@gmail.com>
2024-02-22 19:45:14 +0530

    update

---
+++ WhatsApp/checkForNewMessages.php
@@ -0,0 +1,47 @@
+<?php
+include("../connection.php");
+require("../encryption.php");
+
+if($_SERVER['REQUEST_METHOD'] == 'GET'){
+    $headers = getallheaders();
+    
+    $authorizationHeader = trim($headers['Authorization']);
+
+	if (array_key_exists('Authorization', $headers) && preg_match('/Bearer\s(\S+)/', $authorizationHeader, $matches)){
+		if(verifyToken($matches[1])){
+
+            $UserID = $LoginUserID;
+
+
+            $CheckNew= mysqli_fetch_assoc(mysqli_query($conn, "SELECT COUNT(isRead) as NewMsg FROM `whatsapp_messages` WHERE isRead = 0"));
+            if($CheckNew['NewMsg']==0){
+                $data = false;
+            }else{
+                $data = true;
+            }
+            $data = array ("Result" => $data);
+            response(200, $data);
+
+		}else{
+			
+            $data = array ("Message" => "Invalid Token");
+            response(401, $data);
+
+		}
+
+	}else{
+
+		$data = array ("Message" => "Token Missing");
+        response(401, $data);
+
+	}
+
+	
+}else{
+
+    $data = array ("Message" => "Method Not Allowed");
+    response(405, $data);
+	
+}
+
+?>
