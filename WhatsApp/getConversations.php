4f4d516bc74af77483890fc440e3da26b857ac0a
Gordhan Suthar <gdnagal1536@gmail.com>
2024-02-22 09:44:07 +0530

    update

---
+++ WhatsApp/getConversations.php
@@ -0,0 +1,45 @@
+<?php
+include("../connection.php");
+require("../encryption.php");
+
+if($_SERVER['REQUEST_METHOD'] == 'GET'){
+    $headers = getallheaders();
+    
+    $authorizationHeader = trim($headers['Authorization']);
+
+	if (array_key_exists('Authorization', $headers) && preg_match('/Bearer\s(\S+)/', $authorizationHeader, $matches)){
+		if(verifyToken($matches[1])){
+
+            $UserID = $LoginUserID;
+            $ConversationArr = [];
+
+            $ConversationList = mysqli_query($conn, "SELECT * FROM `whatsapp_conversation`");
+            while($PCRow = mysqli_fetch_assoc($ConversationList)){
+                $ConversationArr[] = $PCRow;
+            }
+            $data = array ("ConversationList" => $ConversationArr);
+            response(200, $data);
+
+		}else{
+			
+            $data = array ("Message" => "Invalid Token");
+            response(401, $data);
+
+		}
+
+	}else{
+
+		$data = array ("Message" => "Token Missing");
+        response(401, $data);
+
+	}
+
+	
+}else{
+
+    $data = array ("Message" => "Method Not Allowed");
+    response(405, $data);
+	
+}
+
+?>
