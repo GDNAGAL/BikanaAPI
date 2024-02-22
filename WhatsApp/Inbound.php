c357a1978810811dccbe71b86793a0887ae4a1eb
Gordhan Suthar <gdnagal1536@gmail.com>
2024-02-22 13:24:22 +0530

    update

--- WhatsApp/Inbound.php
+++ WhatsApp/Inbound.php
@@ -15,10 +15,10 @@ if($_SERVER['REQUEST_METHOD']=="GET"){
         $bussiness_number = $metadata['display_phone_number'];
         
         foreach($contacts as $contact){
-            file_put_contents("response.txt","ok");
             $wa_id = $contact['wa_id'];
             $profile_Name =  $contact['profile']['name'];
             $CountConversation = mysqli_fetch_assoc(mysqli_query($conn, "SELECT Count(wa_id) FROM `whatsapp_conversation` WHERE wa_id = '$wa_id'"));
+            file_put_contents("response.txt",$CountConversation);
             if($CountConversation['wa_id']==0){
                 mysqli_query($conn,"INSERT INTO `whatsapp_conversation`(`Bussiness_Number`, `wa_id`, `profile_Name`, `Conversation_Status`, `Created_At`, `Modified_At`)
                  VALUES ('$bussiness_number','$wa_id','$profile_Name','1','$CurrendDateTime','$CurrendDateTime')");
