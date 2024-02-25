ffcfe9cc2f336740dd8e28f5df1664c16d40621b
Gordhan Suthar <gdnagal1536@gmail.com>
2024-02-25 17:48:03 +0530

    update

--- Labels/addLabel.php
+++ Labels/addLabel.php
@@ -12,6 +12,20 @@ if($_SERVER['REQUEST_METHOD'] == 'GET'){
             $wa_id = $_GET['wa_id'];
             $label = $_GET['label'];
 
+            $checkforduplicate = mysqli_fetch_assoc(mysqli_query($conn,"SELECT * FROM `conversation_label` WHERE wa_id = '$wa_id'"));
+            $rows = mysqli_num_rows($checkforduplicate);
+            if($rows>0){
+                $lastinsert = new DateTime($checkforduplicate['Created_At']);
+                $currentTime = new DateTime();
+                $timeDifference = $givenTime->diff($currentTime);
+                $hourDifference = $timeDifference->h;
+                if($hourDifference<20){
+                    $data = array ("Message" => "Label Already Added");
+                    response(200, $data);
+                    exit();
+                }
+
+            }
             mysqli_query($conn, "INSERT INTO `conversation_label`(`wa_id`, `label`, `Created_At`, `Created_By`) VALUES ('$wa_id','$label','$CurrendDateTime','$UserID')");
             $data = array ("Message" => "Label Added Successfully");
             response(200, $data);
