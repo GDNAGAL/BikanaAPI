9e4ad8a74210b98acd68bd34921d3ff7b9a02660
Gordhan Suthar <gdnagal1536@gmail.com>
2024-02-25 15:45:07 +0530

    update

--- WhatsApp/Inbound.php
+++ WhatsApp/Inbound.php
@@ -58,86 +58,7 @@ if($_SERVER['REQUEST_METHOD']=="GET"){
                 $sha256 = $message['image']['sha256'];
                 $iid = $message['image']['id'];
 
-                // mysqli_query($conn,"INSERT INTO `whatsapp_messages`(`WhatsappMobile`, `Type`, `FromOrTo`, `WaMID`, `MessageTimeStamp`, `MessageType`, `ContextFrom`, `ContextID`, `isRead`) 
-                //  VALUES ('$bussiness_number','RECEIVED','$from','$WaMID','$timestamp','$messageType'," . ($contextFrom !== null ? "'$contextFrom'" : "NULL") . "," . ($contextID !== null ? "'$contextID'" : "NULL") . ",0)");
-                // $MessageID = mysqli_insert_id($conn);
-                
-
-                $curl = curl_init();
-
-                curl_setopt_array($curl, array(
-                CURLOPT_URL => 'https://graph.facebook.com/v18.0/'.$iid,
-                CURLOPT_RETURNTRANSFER => true,
-                CURLOPT_ENCODING => '',
-                CURLOPT_MAXREDIRS => 10,
-                CURLOPT_TIMEOUT => 0,
-                CURLOPT_FOLLOWLOCATION => true,
-                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-                CURLOPT_CUSTOMREQUEST => 'GET',
-                CURLOPT_HTTPHEADER => array(
-                    'Authorization: Bearer EAAEzKRKZAiY0BOxJo1Gq9JZA9kafrKJjLmaI5RYpxv1yZBbtuR8KNJVRGu43RKH1157ZCG2QJZBrPl7SCQZBuMy3YhZCqBKar3FRJD9oN6QiIGxcY33R5QwrsHprMou1pT8pMNHHdv0QQjFoxDngwUyfveDnsh5Uv4h0gDZAdZBsuocU3fvQE3W3KFZACImCMGJtgR',
-                ),
-                ));
-
-                $response = curl_exec($curl);
-                $httpStatus = curl_getinfo($curl, CURLINFO_HTTP_CODE);
-                curl_close($curl);
-
-                if ($httpStatus == 200) {
-                    // Decode JSON response
-                    $data = json_decode($response, true);
-                    // Check for errors in the response
-                    if (isset($data['error'])) {
-                        die('API Error: ' . $data['error']['message']);
-                    }
-                    $imageUrl = $data['url'];
-                    $mime_type = $data['mime_type'];
-                    $extension = '';
-                    switch ($mime_type) {
-                        case 'image/jpeg':
-                            $extension = 'jpg';
-                            break;
-                        case 'image/png':
-                            $extension = 'png';
-                            break;
-                        case 'image/gif':
-                            $extension = 'gif';
-                            break;
-                        // Add more cases as needed for other image types
-                        default:
-                            // Default to a generic extension if the MIME type is not recognized
-                            $extension = 'unknown';
-                    }
-                    
-                    $curle = curl_init();
-                    curl_setopt_array($curle, array(
-                        CURLOPT_URL => $imageUrl,
-                        CURLOPT_RETURNTRANSFER => true,
-                        CURLOPT_ENCODING => '',
-                        CURLOPT_MAXREDIRS => 10,
-                        CURLOPT_TIMEOUT => 0,
-                        CURLOPT_FOLLOWLOCATION => true,
-                        CURLOPT_FOLLOWLOCATION => true,
-                        CURLOPT_USERAGENT => "BikanWallMart/2.0",
-                        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-                        CURLOPT_CUSTOMREQUEST => 'GET',
-                        CURLOPT_HEADER => true,
-                        CURLOPT_HTTPHEADER => array('Authorization: Bearer EAAEzKRKZAiY0BOxJo1Gq9JZA9kafrKJjLmaI5RYpxv1yZBbtuR8KNJVRGu43RKH1157ZCG2QJZBrPl7SCQZBuMy3YhZCqBKar3FRJD9oN6QiIGxcY33R5QwrsHprMou1pT8pMNHHdv0QQjFoxDngwUyfveDnsh5Uv4h0gDZAdZBsuocU3fvQE3W3KFZACImCMGJtgR'),
-                    ));
-                    $result = curl_exec($curle); 
-                    curl_close($curle);
-                    list($headers, $content) = explode("\r\n\r\n", $result, 2);
-        
-                    foreach (explode("\r\n", $headers) as $hdr) {
-                        if ( strpos($hdr, "Content-Disposition") !== false ) { $ext = substr(strrchr($hdr, '.'), 1); }
-                    }
-                    $file_loc   = "../Data/" . $iid . "." . $extension ;
-                    $fp         = fopen($file_loc, 'wb');
-                    fwrite($fp, $content); fclose($fp);
-                    
-                }
-                
-                $file_location = "https://groceryapi.royalplay.live/Data/".$iid.".".$extension;
+                $file_location = DownloadMedia($iid);
 
                 mysqli_query($conn,"INSERT INTO `whatsapp_image_messages`(`MessageID`, `Caption`, `mime_type`, `sha256`, `iid`, `image_path`)
                  VALUES ('$MessageID','$caption','$mime_type','$sha256','$iid','$file_location')");
@@ -147,14 +68,84 @@ if($_SERVER['REQUEST_METHOD']=="GET"){
 
             
         }
-
-       
-        
-        
-        // Now you can use these variables as needed in your application logic
-        // ...
     
     }
 }
 
+function DownloadMedia($mediaID){
+    $curl = curl_init();
+
+    curl_setopt_array($curl, array(
+    CURLOPT_URL => 'https://graph.facebook.com/v18.0/'.$mediaID,
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_ENCODING => '',
+    CURLOPT_MAXREDIRS => 10,
+    CURLOPT_TIMEOUT => 0,
+    CURLOPT_FOLLOWLOCATION => true,
+    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+    CURLOPT_CUSTOMREQUEST => 'GET',
+    CURLOPT_HTTPHEADER => array(
+        'Authorization: Bearer EAAEzKRKZAiY0BOxJo1Gq9JZA9kafrKJjLmaI5RYpxv1yZBbtuR8KNJVRGu43RKH1157ZCG2QJZBrPl7SCQZBuMy3YhZCqBKar3FRJD9oN6QiIGxcY33R5QwrsHprMou1pT8pMNHHdv0QQjFoxDngwUyfveDnsh5Uv4h0gDZAdZBsuocU3fvQE3W3KFZACImCMGJtgR',
+    ),
+    ));
+
+    $response = curl_exec($curl);
+    $httpStatus = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+    curl_close($curl);
+
+    if ($httpStatus == 200) {
+        // Decode JSON response
+        $data = json_decode($response, true);
+        // Check for errors in the response
+        if (isset($data['error'])) {
+            die('API Error: ' . $data['error']['message']);
+        }
+        $imageUrl = $data['url'];
+        $mime_type = $data['mime_type'];
+        $extension = '';
+        switch ($mime_type) {
+            case 'image/jpeg':
+                $extension = 'jpg';
+                break;
+            case 'image/png':
+                $extension = 'png';
+                break;
+            case 'image/gif':
+                $extension = 'gif';
+                break;
+            // Add more cases as needed for other image types
+            default:
+                // Default to a generic extension if the MIME type is not recognized
+                $extension = 'unknown';
+        }
+        
+        $curle = curl_init();
+        curl_setopt_array($curle, array(
+            CURLOPT_URL => $imageUrl,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_USERAGENT => "BikanWallMart/2.0",
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'GET',
+            CURLOPT_HEADER => true,
+            CURLOPT_HTTPHEADER => array('Authorization: Bearer EAAEzKRKZAiY0BOxJo1Gq9JZA9kafrKJjLmaI5RYpxv1yZBbtuR8KNJVRGu43RKH1157ZCG2QJZBrPl7SCQZBuMy3YhZCqBKar3FRJD9oN6QiIGxcY33R5QwrsHprMou1pT8pMNHHdv0QQjFoxDngwUyfveDnsh5Uv4h0gDZAdZBsuocU3fvQE3W3KFZACImCMGJtgR'),
+        ));
+        $result = curl_exec($curle); 
+        curl_close($curle);
+        list($headers, $content) = explode("\r\n\r\n", $result, 2);
+
+        foreach (explode("\r\n", $headers) as $hdr) {
+            if ( strpos($hdr, "Content-Disposition") !== false ) { $ext = substr(strrchr($hdr, '.'), 1); }
+        }
+        $file_loc   = "../Data/" . $iid . "." . $extension ;
+        $fp         = fopen($file_loc, 'wb');
+        fwrite($fp, $content); fclose($fp);
+        return "https://groceryapi.royalplay.live/Data/".$iid.".".$extension;
+    }
+    return null;
+}
 ?>
