012b193a1e39b1f018cc478b8e17f97d7cabfd95
Gordhan Suthar <gdnagal1536@gmail.com>
2024-02-25 18:20:07 +0530

    update

--- Labels/addLabel.php
+++ Labels/addLabel.php
@@ -11,6 +11,7 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
             $UserID = $LoginUserID;
             $wa_id = $_POST['wa_id'];
             $label = $_POST['label'];
+            $b_class = $_POST['b_class'];
 
             $q = mysqli_query($conn,"SELECT * FROM `conversation_label` WHERE wa_id = '$wa_id'");
             $checkforduplicate = mysqli_fetch_assoc($q);
@@ -27,7 +28,7 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
                 }
 
             }
-            mysqli_query($conn, "INSERT INTO `conversation_label`(`wa_id`, `label`, `Created_At`, `Created_By`) VALUES ('$wa_id','$label','$CurrendDateTime','$UserID')");
+            mysqli_query($conn, "INSERT INTO `conversation_label`(`wa_id`, `label`, `Created_At`, `Created_By`, `b_class`) VALUES ('$wa_id','$label','$CurrendDateTime','$UserID','$b_class')");
             $data = array ("Message" => "Label Added Successfully");
             response(200, $data);
 
